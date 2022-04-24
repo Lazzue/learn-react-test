d613443e68c96fe756ef3354d65e0c13124d22ec
Lazuardi Pradipta Kasidyan <89512496+Lazzue@users.noreply.github.com>
2022-04-24 11:35:53 +0700

    Update Ternary

--- app.jsx
+++ app.jsx
@@ -6,6 +6,8 @@ const App = () => {
   const [card, setCard] = useState([])
   const [selectedHero, setSelectedHero] = useState("Select your hero above 👆🏼")
   const [selectedCard, setSelectedCard] = useState()
+  const [selectedSkills, setSelectedSkills] = useState([false])
+  const [skillsCard, setSkillsCard] = useState()
 
   // Fetch API, format JSX
   useEffect(() => {
@@ -22,28 +24,42 @@ const App = () => {
   const skillsClick = async () => {
     const response = await fetch(`https://materi-thrive-demo.vercel.app/api/hero/${selectedCard}`)
     const dataHeroes = await response.json()
-    console.log('response', dataHeroes);
-  }
+    const skillsHeroes = dataHeroes.skills;
+    console.log('response', skillsHeroes);
+    setSelectedSkills(true);
+    setSkillsCard(skillsHeroes);
+  };
 
-  const handleClick = (name, id) => {
+  const handleClick = async (name, id) => {
     setSelectedHero("See " + name + "'s skills")
     setSelectedCard(id)
-  }
+  };
 
   return (
     <div className="container">
-      {loading ? (<h1>Loading.... Calling your heroes....</h1>) : (card.map(function (hero) {
-        return (
-          <div key={hero.id} className={hero.id == selectedCard ? "card-active" : "card"}>
-            <img className="image" src={hero.imgSrc}/>
-            <h1>{hero.name}</h1>
-            <h2>{hero.age}</h2>
-            <h3>{hero.origin}</h3>
-            <button onClick={() => handleClick(hero.name, hero.id)}>Select</button>
-          </div>
-        )
-      }))}
-      {!loading && <button onClick={() => skillsClick(selectedCard)}>{selectedHero}</button>}
+      {loading ? (<h1>Loading.... Calling your heroes....</h1>) : selectedSkills ? (
+        (card.map(function (hero) {
+          return (
+            <div key={hero.id} className={hero.id == selectedCard ? "card-active" : "card"}>
+              <img className="image" src={hero.imgSrc}/>
+              <h1>{hero.name}</h1>
+              <h2>{hero.age}</h2>
+              <h3>{hero.origin}</h3>
+              <button className="btn" onClick={() => handleClick(hero.name, hero.id)}>Select</button>
+            </div>
+          )
+        }))
+      ) : (
+        (skillsCard.map(function (skill){
+          return (
+            <div className="card">
+              <img className="image" src={skill.imgSrc}/>
+              <h1>{skill.name}</h1>
+            </div>
+          )
+        }))
+      )}
+      {!loading && <button className="btn" onClick={() => skillsClick(selectedCard)}>{selectedHero}</button>}
     </div>
   )
 };
